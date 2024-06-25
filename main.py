5224db53eae704f89fa46bdf211048e3315926e0
alwaz <chalwaz10315@gmail.com>
2024-06-25 19:31:24 +0500

    arg based to cli

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
 import sys
-import argparse
 import pyfiglet
 
 def set_git_user(username, email):
@@ -21,22 +20,16 @@ def show_logo():
     print(ascii_art)
 
 def main():
-    parser = argparse.ArgumentParser(description="Manage Git global user configuration.")
-    parser.add_argument("--set", nargs=2, metavar=("username", "email"), help="Set Git global user. If no arguments are provided, the tool will prompt for username and email.")
-    parser.add_argument("--view", action="store_true", help="View Git global user configuration.")
-
-    args = parser.parse_args()
-
     show_logo()
 
-    if args.set:
-        username, email = args.set
-        set_git_user(username, email)
-        print("Git global user configuration has been set.")
-    elif args.view:
-        view_git_config()
-    else:
-        parser.print_help()
+    # Prompting for username and email
+    username = input("Enter your Git username: ")
+    email = input("Enter your Git email: ")
+
+    set_git_user(username, email)
+    print("Git global user configuration has been set.")
+
+    # Optionally, you can add a menu or another option here to allow the user to perform other actions like viewing the config
 
 if __name__ == "__main__":
     main()
